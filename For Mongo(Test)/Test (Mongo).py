02ddf280bf64703ad3ca78a94974ed88eec1c04a
AnakinP0 <se11010001@gmail.com>
2024-04-23 17:09:42 +0500

    saving username link  (added)

--- For Mongo(Test)/Test (Mongo).py
+++ For Mongo(Test)/Test (Mongo).py
@@ -4,10 +4,8 @@ import asyncio
 import os
 from datetime import datetime
 
-
 MONGODB_URI = os.getenv('MONGODB_URI')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
@@ -15,27 +13,31 @@ dp = Dispatcher()
 cluster = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URI)
 collection = cluster.DreamBot.User_Auth
 
-async def add_user(user_id, username):
+async def add_user(user_id, username, telegram_username):
     date = datetime.now().date()
+    user_data = {
+        "_id": user_id,
+        "date": str(date),
+        "name": str(username)
+    }
+    if telegram_username:
+        user_data["telegram_username"] = "@" + str(telegram_username)
+
     existing_user = await collection.find_one({"_id": user_id})
     if not existing_user:
-        await collection.insert_one({
-            "_id": user_id,
-            "date": str(date),
-            "username": str(username)
-        })
+        await collection.insert_one(user_data)
     else:
         print("Пользователь уже существует")
-        # Позже можно сделать функцию update_user, чтобы обновлять инфу о юзерах которые снова используют /start
-
+        # Здесь можно добавить логику для обновления данных пользователя, если это необходимо
 
 @dp.message()
 async def handle_messages(message: types.Message):
     if message.text and message.text.startswith('/start'):
         await bot.send_message(message.chat.id, "Привет, какой сон тебе приснился?")
         user_id = message.chat.id
-        username = message.from_user.first_name
-        await add_user(user_id, username)
+        name = message.from_user.first_name
+        telegram_username = message.from_user.username  # Здесь извлекается username пользователя
+        await add_user(user_id, name, telegram_username)
 
 async def main():
     # Начать поллинг событий
@@ -43,6 +45,5 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
 
 
