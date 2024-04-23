6c9c3dfbc4d6166f3b7a8847ad2eee2d93baf8e6
AnakinP0 <se11010001@gmail.com>
2024-04-23 16:57:05 +0500

    user name (not link)

--- For Mongo(Test)/Test (Mongo).py
+++ For Mongo(Test)/Test (Mongo).py
@@ -15,17 +15,18 @@ dp = Dispatcher()
 cluster = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URI)
 collection = cluster.DreamBot.User_Auth
 
-async def add_user(user_id):
+async def add_user(user_id, username):
     date = datetime.now().date()
     existing_user = await collection.find_one({"_id": user_id})
     if not existing_user:
         await collection.insert_one({
             "_id": user_id,
-            "date": str(date)
+            "date": str(date),
+            "username": str(username)
         })
     else:
         print("Пользователь уже существует")
-        # Позже можно сделать функцию update_user, чтобы обновлять инфу о юзерах которые снова используют /start 
+        # Позже можно сделать функцию update_user, чтобы обновлять инфу о юзерах которые снова используют /start
 
 
 @dp.message()
@@ -33,7 +34,8 @@ async def handle_messages(message: types.Message):
     if message.text and message.text.startswith('/start'):
         await bot.send_message(message.chat.id, "Привет, какой сон тебе приснился?")
         user_id = message.chat.id
-        await add_user(user_id)
+        username = message.from_user.first_name
+        await add_user(user_id, username)
 
 async def main():
     # Начать поллинг событий
